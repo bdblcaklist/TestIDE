2323fc81d7b1e5cffc0ced3d4044b8314a6c25d5
bdblacklist <motiurny@gmail.com>
2023-03-04 11:49:28 -0500

    Added one new class and made some to other class

---
+++ JavaFeb23/src/actionItem/AddinNumbers.java
@@ -0,0 +1,28 @@
+package actionItem;
+
+public class AddinNumbers {
+
+	public static void main(String[] args) {
+		
+		int sum =0;
+		for(int i= 5; i<=15; i++) {
+			sum = sum +i;
+			
+		}
+		System.out.println("The result is: " + sum);
+     
+		int sum1 = 0;
+        int i =0;
+        while (i<=15) {
+    	 
+    	 sum1 = sum1 +i;
+    	 i++;
+    	 
+     }
+     System.out.println("The result is: " + sum1);
+     
+	}
+	
+	
+
+}
---
+++ JavaFeb23/src/array/ArrayDemo.java
@@ -0,0 +1,67 @@
+package array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		//Declaring Array
+		//String [] classTwo = new String[3];
+		//Assinging value to the array
+		 
+		//classTwo[0] = "Selenium";
+		//classTwo[1] = "Eclipse";
+		//classTwo[2] = "Java";
+		 
+		 //System.out.println(classTwo[0]);
+		 
+		 //for(String s: classTwo) {
+			 //System.out.println(s);
+		 
+		 /*for(int i = 0;i<classTwo.length; i++) {
+			 System.out.println(classTwo[i]);
+		 }
+		 
+		 // Another way of declaring the array
+		 int [] number = {100,101,102};
+		 for (int i= 0; i<number.length; i++) {
+			 System.out.println(number[i]);
+		 }
+		 
+		 System.out.println("Example of Enhanced for loop");
+		 // example of Enhanced for loop
+		 
+		 for(int val: number) {
+			 System.out.println(val);
+		 }
+		
+
+				 String [] testArray = new String[5];
+		
+	     testArray[0] = "Toyota";
+		 testArray [1] = "Nissan";
+		 testArray [2] = "Ram";
+		 testArray [3] = "Tesla";
+		 testArray [4] = "Mitsubishi";
+		 
+		 System.out.println(testArray[4]);
+		 
+		String [] cars;
+		String [] 	cars1 = {"BMW", "VOLVO", "FORD", "MAZDA"
+	};
+		System.out.println(cars1[2]);
+		
+		
+		int [] myNum = {10,20,30,40};
+		System.out.println(myNum.length);
+		*/
+		String [] cars;
+		String [] 	cars1 = {"BMW", "VOLVO", "FORD", "MAZDA"
+	};
+	cars1 [1] = "Suzuki";
+	System.out.println(cars1[1]);
+
+	
+
+}
+}
+
+
